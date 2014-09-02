42a75d3439678e8674cb2aba06241b3eb6070f34
Rafael Miceli <rafael.miceli@hotmail.com>
2014-09-02 17:18:43 -0300

    testando aplicação web

---
+++ ListaComprasWeb.Tests/ProdutoControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ListaComprasWeb.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ListaCompras.Domain.Model;
+
+namespace ListaComprasWeb.Tests
+{
+    [TestClass]
+    public class ProdutoControllerTests
+    {
+        [TestMethod]
+        public void Given_A_Request_To_Create_A_Product_When_Create_Product_Then_Create_Product_With_No_Errors()
+        {
+            var produto = new Produto("Beterraba", 1, "");
+
+            var produtoController = new ProdutoController();
+
+            var resultado = produtoController.Create(produto);
+
+            Assert.IsNotNull(resultado);
+        }
+    }
+}
